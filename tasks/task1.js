504f31432c8d4ac79cfbaf328f83c50f409fd610
Kenpachi2 <ifeanyiumeadi201@gmail.com>
2023-06-30 07:11:07 -0700

    add files

---
+++ tasks/task1.js
@@ -0,0 +1,12 @@
+
+// Create an array of numbers
+var numbers = [1, 2, 3, 4, 5];
+
+// Use a "for" loop to iterate over the array
+for (var i = 0; i < numbers.length; i++) {
+  // Log each number to the console
+  console.log(numbers[i]);
+}
+
+
+
